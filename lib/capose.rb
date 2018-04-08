de026ee9b7312b5c516c26c07d08675851e144f6
Marino Bonetti <marinobonetti@gmail.com>
2018-04-08 09:17:19 +0200

    first test

--- lib/capose.rb
+++ lib/capose.rb
@@ -1,17 +1,19 @@
+require 'json'
 namespace :deploy do
   after :updated, 'capose:deploy'
+  after :cleanup, 'capose:cleanup'
 end
 
 namespace :capose do
   def _project
     return "" if fetch(:capose_project).nil?
-    "-p #{fetch(:capose_project)}"
+    "-p #{fetch(:capose_project)}_tmp"
   end
 
   def _files
     cmd = []
 
-    Array(fetch(:capose_file)).each { |file| cmd << ["-f #{file}"] }
+    Array(fetch(:capose_file)).each {|file| cmd << ["-f #{file}"]}
 
     cmd.join(" ")
   end
@@ -51,12 +53,68 @@ namespace :capose do
       end
     end
 
+    base_image_name = "#{fetch(:capose_project)}:#{fetch(:capose_image_tag)}"
+    compose_override = "docker-compose-image-override.yml"
+
+    ## costruiamo l'immagine
+    on roles(fetch(:capose_role)) do
+      within release_path do
+        with fetch(:capose_env) do
+
+
+          execute :docker, :build, ". -t #{base_image_name}"
+
+          compose_production = {
+            version: '3',
+            services: {
+              fetch(:capose_service_to_build,'app').to_sym=> {
+                image: base_image_name
+              }
+            }
+          }
+
+          contents = StringIO.new(JSON[compose_production.to_json].to_yaml)
+          upload! contents, "#{release_path}/#{compose_override}"
+
+          set :capose_file, fetch(:capose_file) + [compose_override]
+
+
+        end
+      end
+    end
+
+
     on roles(fetch(:capose_role)) do
       within release_path do
         with fetch(:capose_env) do
           fetch(:capose_commands).each do |command|
             execute :"docker-compose", _command(command)
           end
+
+          execute :docker, :stack, :deploy, "-c #{compose_override} #{fetch(:capose_project)}"
+
+        end
+      end
+    end
+  end
+
+
+  task :cleanup do
+    on roles(fetch(:capose_role)) do
+      within release_path do
+        with fetch(:capose_env) do
+          tags_list = capture :docker, :images, fetch(:capose_project).to_sym, '--format "{{.Tag}}"'
+
+          list = tags_list.split
+          list.delete('latest')
+
+          releases = capture(:ls, "-x", releases_path).split
+
+          (list - releases).each do |r|
+            #delete image without release path
+            execute :docker, :image, :rm, "#{fetch(:capose_project)}:#{r}"
+          end
+
         end
       end
     end
@@ -65,12 +123,14 @@ end
 
 namespace :load do
   task :defaults do
-    set :capose_role,    -> { :web }
-    set :capose_copy,    -> { [] }
-    set :capose_project, -> { fetch(:application) }
-    set :capose_file,    -> { ["docker-compose-#{fetch(:stage)}.yml"] }
-    set :capose_env,     -> { {} }
+    set :capose_role, -> {:web}
+    set :capose_copy, -> {[]}
+    set :capose_project, -> {fetch(:application)}
+    set :capose_file, -> {["docker-compose-#{fetch(:stage)}.yml"]}
+    set :capose_env, -> {{}}
+    set :capose_image_tag, -> {fetch(:release_timestamp)}
+    set :capose_service_to_build, -> {'app'}
 
-    set :capose_commands, -> { ["build", "up -d"] }
+    set :capose_commands, -> {["build", "up -d"]}
   end
 end
